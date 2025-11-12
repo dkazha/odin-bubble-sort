0444b3b83e0318dd0626303fac5b0817c335b142
Davis Kaza <156680027+dkazha@users.noreply.github.com>
2025-11-12 15:46:29 +0100

    Added loops

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,9 +1,17 @@
-def bubble_sort(unsorted_array)
-  x = unsorted_array.length
-
+def bubble_sort(array)
+  x = array.length
+  p "x = #{x}"
   for i in 0..(x - 1) do
-    p i
+    p "i = #{i}"
+    for j in 0..(x - i - 1)
+      p "j = #{j}"
+      if array[j] > array[(j + 1)]
+        array[j], array[(j + 1)] = array[(j + 1)], array[j]
+        p "one: #{array[j]} two: #{array[(j + 1)]}"
+      end
+    end
   end
+  puts array
 end
 
 bubble_sort([4,3,78,2,0,2])
